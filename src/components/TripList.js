5c68d1606a9b6bc5dca8762fa505650737a0024e
Waleed <wsaleh1991@gmail.com>
2021-11-02 01:13:22 -0500

    Fetching data refresher

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,34 +1,26 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 
 // styles
 import "./TripList.css";
 
 export const TripList = () => {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
-
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    setTrips(json);
-  }, [url]);
-
-  useEffect(() => {
-    fetchTrips();
-  }, [url, fetchTrips]);
-
-  console.log(trips);
+  const { data: trips, isPending, error } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
+      {isPending && <div>Loading trips...</div>}
+      {error && <div>{error}</div>}
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className="filters">
         <button
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const json = await response.json();
+
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        setIsPending(false);
+        setError("Could not fetch data");
+        console.log(err.message);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isPending, error };
+};
